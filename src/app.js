fe592f363a25d78342cb5489f12ec7e530e132fc
jignesh <85551495+jigneshbhai@users.noreply.github.com>
2023-12-09 15:07:10 +0530

    Register controller done

--- src/app.js
+++ src/app.js
@@ -17,6 +17,10 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+//routes
+import userRouter from './routes/userRoute.js'
+
+app.use("/api/v1/users", userRouter)
 
 
 
---
+++ src/controllers/userController.js
@@ -0,0 +1,63 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/userModel.js";
+import { uploadOnCloudinary } from "../utils/cloudnary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { fullName, email, username, password } = req.body;
+
+  if (
+    [fullName, email, username, password].some((field) => field.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are require");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (existedUser) {
+    throw new ApiError(409, "User already exist");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is require");
+  }
+
+  //upload on cloudinary this image
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is require");
+  }
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (createdUser) {
+    throw new ApiError(500, "Something went wrong while register user");
+  }
+
+  return res.status(201).json(
+    new ApiResponse(200, createdUser, "User successfully registered")
+  )
+
+});
+
+export { registerUser };
